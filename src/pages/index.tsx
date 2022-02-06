5c9a5c56664e5971c6da7e39704a35c2a5c99fbf
Nayanajith Gajasinghe <it18153682@my.sliit.lk>
2022-02-06 12:49:00 +0530

    update ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,10 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import {Navbar} from "../components/navbar";
 
 const Home: NextPage = () => {
   return (
       <div>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
-          </h1>
+          <Navbar/>
       </div>
   )
 }
